121d26f3304552661f8f72d9dfbf201997c14ec8
Mary Kang <mary.kang09@gmail.com>
2020-03-05 21:54:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,3 @@
 def nyc_pigeon_organizer(data)
- data.each_with_object({}) do |(key, h), result| 
-#data.each_with_object ({}) do |(key,hash), info|
+data.each_with_object ({}) do |(key,hash), info|
 end
