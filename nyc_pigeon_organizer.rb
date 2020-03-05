244b3372d85d03b668b269df266f52467a27a29a
Mary Kang <mary.kang09@gmail.com>
2020-03-05 22:03:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+data.each_with_object ({}) do |(key,hash), info|
+  hash.each do |value, names|
+    names.each do |name|
+      info[name] ||={}
+      info[name][key] ||= []
+      info[name][key] << value.to_s 
+    end
+end
+end
 end
